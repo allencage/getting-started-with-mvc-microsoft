8b23f26a7af5e64145d56796ad1a7f471044b8ee
allencage <alingabriel.ciuca@gmail.com>
2018-03-16 15:53:25 +0200

    Initial commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View(); 
+        }
+
+        public string Welcome(string name, int id)
+        {
+            //Note - use HttpUtility to encode the items you receive as parameters because you could receive malicious javascript
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = id;
+
+            return View();
+        }
+
+    }
+}
